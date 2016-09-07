c3dff26d7dd6caf616aad92e076b41a69db0b09b
Austyn Guo <austynguo@gmail.com>
2016-09-07 23:54:42 +1000

    Added node_modules folder to list of static paths

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Extra places for collect static to find static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "node_modules") # Add node_modules folder
+]
