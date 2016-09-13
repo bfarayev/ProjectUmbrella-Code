ba64dec4c359eb76fa70448c090785593f986f25
Austyn Guo <austynguo@gmail.com>
2016-09-13 11:01:07 +1000

    Fixed minor link issue

--- umbrella/urls.py
+++ umbrella/urls.py
@@ -8,6 +8,6 @@ app_name = "umbrella"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^signin/', views.signin, name='signin'),
-    url(r'^googlemap',views.googlemap,name='googlemap')
+    url(r'^googlemap/',views.googlemap,name='googlemap')
 ]
 
