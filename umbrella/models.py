96d1bb68dd4c1405c02d6eea50c9f26edd2c320e
Jingkai Meng <jingkaimeng@Jingkais-MacBook-Pro.local>
2016-09-14 23:00:06 +1000

    Fixed model.py bug, umbrella app can be seen in /admin site

--- umbrella/models.py
+++ umbrella/models.py
@@ -1,5 +1,8 @@
-from django.db import models
+# from django.db import models
+# How about following one?
+from django.contrib.gis.db import models
 from django.contrib.postgres.fields import ArrayField
+
 
 class Category(models.Model):
     title = models.CharField(max_length=10)
@@ -11,16 +14,18 @@ class Category(models.Model):
         _category = cls(title=category_name)
         return _category
 
+
 # Create Location class
 class Location(models.Model):
     latitude = models.FloatField(default=-33.888584)
     longitude = models.FloatField(default=151.1851586)
 
+
 # Create the Posts class
 class Post(models.Model):
     content = models.TextField(max_length=40)
     category = models.ForeignKey(Category, null=True, blank=True)
-    location = models.ForeignKey(Location, null=True,blank=True)
+    location = models.ForeignKey(Location, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     @classmethod
@@ -30,12 +35,13 @@ class Post(models.Model):
         _location.save()
 
         # Create a category and set it "Uncategorized"
-        _category = Category
+        _category = Category()
         _category.create("Uncategorized")
-        -_category.save()
+        _category.save()
 
         # Now create a post
         post = Post()
         post.location = _location
         post.content = "Sample Post Content"
         post.save()
+
--- umbrella/views.py
+++ umbrella/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from .models import *
 
+
 # Create your views here.
 def index(request):
     # Uncomment below lines to see the post in Postgres DB
-    # post = Post()
-    # post.create()
+    Post.create()
+    latest_post_list = Post.objects.all()
+    context = {'post_list': latest_post_list}
+    return render(request, 'umbrella/index.html', context)
 
-    return render(request, 'umbrella/index.html')
 
 def signin(request):
     return render(request, 'umbrella/signin.html')
 
+
 def googlemap(request):
-    return render(request, 'umbrella/googlemap.html')
+    latest_post_list = Post.objects.all()
+    context = {'post_list': latest_post_list}
+    return render(request, 'umbrella/googlemap.html',context)
