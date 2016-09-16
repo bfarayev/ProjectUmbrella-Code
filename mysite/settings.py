b06865068b00a8874df2840a4c0e970d955ace38
Bakhtiyar <fbakhtiyar@gmail.com>
2016-09-16 17:58:08 +1000

    Travis Please Work

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,7 +87,7 @@ if 'TRAVIS' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'travisci',
+            'NAME': 'umbrella_db',
             'USER': 'postgres',
             'PASSWORD': '',
             'HOST': 'localhost',
