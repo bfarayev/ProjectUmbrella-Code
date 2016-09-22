c7e944a3202b8080dc8a0d32862149a09b6c76a6
Bakhtiyar <fbakhtiyar@gmail.com>
2016-09-22 21:04:07 +1000

    fixing hostname / port issue. rolling back the changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,8 +101,8 @@ else:
             'NAME': get_env_variable('DATABASE_NAME'),
             'USER': get_env_variable('DATABASE_USER'),
             'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
-            'HOST': get_env_variable('DATABASE_HOST'),
-            'PORT': get_env_variable('DATABASE_PORT'),
+            'HOST': '',
+            'PORT': '',
         }
     }
 
